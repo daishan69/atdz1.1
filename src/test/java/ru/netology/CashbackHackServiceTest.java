88b8b215b4fadc6780e1749149e36f8a7aa0636b
Oleg <olegkajlev@gmail.com>
2023-08-08 12:10:11 +0300

    First commit

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -36,7 +36,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
